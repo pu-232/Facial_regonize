8f567d41ba20adabeaeae3a0ed4938e68a4905fd
P123 <csirpro123@gmail.com>
2024-09-30 21:47:00 +0200

    Test

--- facial_impression.py
+++ facial_impression.py
@@ -1,6 +1,6 @@
 from deepface import DeepFace 
 import cv2
-# Git
+
 cam = cv2.VideoCapture(0)
 frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
